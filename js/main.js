ed52a45ba5c1c340ffe02c37ac2840a159f5987b
Adrian Korzan <adrian.korzan@gmail.com>
2019-11-07 21:31:09 +0100

    mechanika kolorów + front

--- js/main.js
+++ js/main.js
@@ -2,86 +2,64 @@ const vm = new Vue({
     el: "#app",
     data:{
 		lastTool : '',
-		tool : 'colorYellow',
+		tool : '',
 		fieldValues : [],
 		win: false,
 		winner: "",
 		ticTocKey: 1,
 		xScore: 0,
 		oScore: 0,
-		tools: ['colorYellow', 'colorGreen']
+		tools: {
+			kitYellow: 'background-color: #a7991d',
+			colorGreen: 'background-color: #22a71d',
+			colorRed: 'background-color: #a71d1d',
+			colorPurple: 'background-color: #901da7',
+			colorBlue: 'background-color: #1d1fa7',
+			colorWhite: 'background-color: #f3eded',
+			colorBlack: 'background-color: #000',
+		},
+		suspects: [
+			'Panna Czerwińska', 
+			'Profesor Śliwiński',
+			'Pani Pawińska',
+			'Ksiądz Zieliński',
+			'Półkownik Żółtkowski',
+			'Pani Bielecka'
+		],
+		weapons: [
+			'Lina',
+			'Świecznik',
+			'Metalowa rurka',
+			'Reworwer',
+			'Sztylet'
+		],
+		rooms: [
+			'Sala Balowa',
+			'Sala Bilardowa',
+			'Jadalnia',
+			'Kuchnia',
+			'Salon',
+			'Hol',
+			'Weranda',
+			'Biblioteka',
+			'Gabinet'
+		]
 	},
 	methods:{
 		activeTool : function(e){
-			e.target
+			let tool = e.target.attributes[0].nodeValue;
+			this.tool = this.tools[tool];
+			this.activeClass(e);
 		},
-		writeXOrO: function(e){
-			if(!this.win && event.target.innerHTML==""){
-				if(this.write == "X"){
-					event.target.innerHTML = "<span>X</span>";
-					this.write = "O";
-					this.checkWinner(event.target);
-				}
-				else{
-					event.target.innerHTML = "<span>O</span>";
-					this.write = "X";
-					this.checkWinner(event.target);
-				}		
+		draw : function(e){
+			e.target.setAttribute('style', this.tool);
+		},
+		activeClass: function(e){
+			let squares = document.querySelectorAll('.kit__square > div');
+			for(let square of squares){
+				square.classList.remove('active');
 			}
-		},
-		checkWinner: function(element){
-			elementId = element.id;
-			elementValue = element.innerText;
-			this.fieldValues[elementId] = elementValue;
-			arr = this.fieldValues;
-			let addScore = (v) =>{
-				this.winner = `Wygrywa ${v}!`;
-				if(v=="X"){
-					this.xScore++;
-				}
-				else{
-					this.oScore++;
-				}
-				this.win = true;
-			}
-
-			if(arr[1]===arr[2] && arr[2]===arr[3] && arr[1]!=undefined){
-				addScore(arr[1]);
-			}else
-			if(arr[4]===arr[5] && arr[5]===arr[6] && arr[4]!=undefined){
-				addScore(arr[4]);
-			}else
-			if(arr[7]===arr[8] && arr[8]===arr[9] && arr[7]!=undefined){
-				addScore(arr[7]);
-			}else
-			if(arr[1]===arr[4] && arr[4]===arr[7] && arr[1]!=undefined){
-				addScore(arr[1]);
-			}else
-			if(arr[2]===arr[5] && arr[5]===arr[8] && arr[2]!=undefined){
-				addScore(arr[2]);
-			}else
-			if(arr[3]===arr[6] && arr[6]===arr[9] && arr[3]!=undefined){
-				addScore(arr[3]);
-			}else
-			if(arr[1]===arr[5] && arr[5]===arr[9] && arr[1]!=undefined){
-				addScore(arr[1]);
-			}else
-			if(arr[3]===arr[5] && arr[5]===arr[7] && arr[3]!=undefined){
-				addScore(arr[3]);
-			}else 
-			if(arr[1]!=undefined && arr[2]!=undefined && arr[3]!=undefined && arr[4]!=undefined && arr[5]!=undefined && arr[6]!=undefined && arr[7]!=undefined && arr[8]!=undefined && arr[9]!=undefined){
-				this.winner="Remis!";
-				// console.log('hej');
-			}
-		},
-		initialState: function(){
-			this.ticTocKey+=1;
-			this.winner = "";
-			this.win = false;
-			this.fieldValues = [];
-		},
-		newGame: function(){
-			this.initialState();
+			e.target.classList.add('active');
 		}
 	}
   });
